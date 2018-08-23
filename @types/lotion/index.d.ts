ef95a6b57a8cd0f6d3415131505bf777a9d4ad43
Aler Denisov <aler.zampillo@gmail.com>
2018-08-23 19:19:17 +0300

    deeply typed Lotion

--- @types/lotion/index.d.ts
+++ @types/lotion/index.d.ts
@@ -1,14 +1,56 @@
 declare module "lotion" {
   type Nullable<T> = { [P in keyof T]: T[P] | null };
-  type MiddlewareFunc<TState, TTransaction> = (
+  type HandlerType = "tx" | "block" | "query" | "initializer" | "post-listen";
+
+  type DefaultChainInfo = {
+    height: number;
+    validators: {
+      [hash: string]: number;
+    };
+  };
+
+  type BlockHandler<TState, TTransaction, TChainInfo> = (
     state: TState,
-    tx: TTransaction
+    info: TChainInfo & DefaultChainInfo
   ) => void;
 
-  interface LotionApp<TState, TTransaction> {
+  type TransactionHandler<TState, TTransaction, TChainInfo> = (
+    state: TState,
+    tx: TTransaction,
+    info: TChainInfo & DefaultChainInfo
+  ) => void;
+
+  type InitializeHandler<TState> = (state: TState) => void;
+
+  type HandlerFunc<TState, TTransaction, TChainInfo> =
+    | TransactionHandler<TState, TTransaction, TChainInfo>
+    | InitializeHandler<TState>
+    | BlockHandler<TState, TTransaction, TChainInfo>;
+
+  type Plugin<TState, TTransaction, TChainInfo> = {
+    type: HandlerType;
+    middleware: HandlerFunc<TState, TTransaction, TChainInfo>;
+  };
+
+  type Middleware<TState, TTransaction, TChainInfo> =
+    | Plugin<TState, TTransaction, TChainInfo>
+    | TransactionHandler<TState, TTransaction, TChainInfo>;
+
+  type Middlewares<TState, TTransaction, TChainInfo> =
+    | Middleware<TState, TTransaction, TChainInfo>
+    | Middleware<TState, TTransaction, TChainInfo>[];
+
+  interface LotionApp<TState, TTransaction, TChainInfo> {
     use(
-      middleware: MiddlewareFunc<TState, TTransaction>
-    ): LotionApp<TState, TTransaction>;
+      middleware: Middlewares<TState, TTransaction, TChainInfo>
+    ): LotionApp<TState, TTransaction, TChainInfo>;
+
+    useTx(
+      txHandler: TransactionHandler<TState, TTransaction, TChainInfo>
+    ): LotionApp<TState, TTransaction, TChainInfo>;
+    useBlock(
+      blockHandler: BlockHandler<TState, TTransaction, TChainInfo>
+    ): LotionApp<TState, TTransaction, TChainInfo>;
 
     listen(port: number): void;
   }
@@ -17,9 +59,14 @@ declare module "lotion" {
     initialState: Nullable<TState>;
   }
 
-  function lotionFunc<TState, TTransaction>(
-    configuration?: ILotionInitialConfiguration<TState>
-  ): LotionApp<TState, TTransaction>;
+  interface ILotionModule {
+    <TState, TTransaction, TChainInfo>(
+      configuration?: ILotionInitialConfiguration<TState>
+    ): LotionApp<TState, TTransaction, TChainInfo>;
 
-  export default lotionFunc;
+    connect(): any;
+  }
+
+  const Lotion: ILotionModule;
+  export = Lotion;
 }
