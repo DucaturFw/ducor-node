dda8d08908813613a049b1f3a3c9f2cc7311d257
Aler Denisov <aler.zampillo@gmail.com>
2018-08-23 18:07:09 +0300

    example initialization of lotion
    Signed-off-by: Aler Denisov <aler.zampillo@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+import lotion from "lotion";
+
+export interface IState {
+  count: number;
+}
+
+export interface ITransaction {}
+
+const app = lotion<IState, ITransaction>({
+  initialState: {
+    count: 0
+  }
+});
+
+app.use((state, tx) => state.count++);
+
+app.listen(3000);
