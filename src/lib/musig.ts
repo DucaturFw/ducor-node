86e1c564236121f7be12b44d07e9aaec74fc4fc9
Aler Denisov <aler.zampillo@gmail.com>
2018-08-27 13:57:27 +0300

    first musig implementation
    Signed-off-by: Aler Denisov <aler.zampillo@gmail.com>

--- src/lib/musig.ts
+++ src/lib/musig.ts
@@ -1,133 +1,99 @@
 import BN = require("bn.js");
-import utils from "./utils";
-import { totalmem } from "os";
+import { hashOfBuffers } from "./utils";
 
-const DRBG = require("bcrypto/lib/drbg");
-const sha256 = require("bcrypto/lib/sha256");
-const elliptic = require("elliptic");
-const curve = elliptic.ec("secp256k1").curve;
-const Signature = require("elliptic/lib/elliptic/ec/signature") as any;
+import elliptic = require("elliptic");
+const secp256k1 = elliptic.ec("secp256k1");
+const Signature = require("elliptic/lib/elliptic/ec/signature");
 
-export type CombinedPublicKeys = Buffer;
-export type GroupFactor = Buffer;
-export default class musig {
-  public static alg = Buffer.from("Schnorr+SHA256  ", "ascii");
+type Point = typeof elliptic.Point;
+type MUSignature = {
+  r: Point;
+  s: BN;
+};
 
-  public static groupFactor(publicKeys: Buffer[]): GroupFactor {
-    publicKeys.forEach(
-      pub => console.assert(pub.length === publicKeys[0].length),
-      "Not same length of pubkeys"
-    );
+export function hashInt(...parts: (Buffer | BN | Point)[]): BN {
+  return new BN(hash(...parts));
+}
 
-    const pub_pool = Buffer.allocUnsafe(
-      publicKeys[0].length * publicKeys.length
-    );
-
-    publicKeys.forEach((pub, index) => pub.copy(pub_pool, index * pub.length));
-    return Buffer.from(sha256.digest(pub_pool), "hex");
-  }
-
-  public static groupPublicKey(
-    publicKeys: Buffer[],
-    factor?: GroupFactor
-  ): any {
-    if (!factor) factor = musig.groupFactor(publicKeys);
-    const publicPoints = publicKeys.map(pub => curve.decodePoint(pub));
-    const memberHashes = publicKeys
-      .map(pub => utils.hashOfBuffers(factor, pub))
-      .map(hash => new BN(hash));
-
-    return Array(publicKeys.length)
-      .fill(0)
-      .reduce((point, index) => {
-        if (!point) {
-          return publicPoints[index].mul(memberHashes[index]);
-        }
-
-        return point.add(publicPoints[index].mul(memberHashes[index]));
-      }, null);
-  }
-
-  public static deterministinK(hash: Buffer, priv: Buffer, salt?: Buffer): any {
-    const pers = Buffer.allocUnsafe(48);
-
-    pers.fill(0);
-
-    if (salt) {
-      console.assert(salt.length === 32);
-      salt.copy(pers, 0);
-    }
-
-    musig.alg.copy(pers, 32);
-
-    return new BN(
-      new DRBG(sha256, priv, hash, pers).generate(curve.n.byteLength())
-    );
-  }
-
-  public static personalRandomPoint(msg: Buffer, key: Buffer, salt?: Buffer) {
-    const r = musig.deterministinK(msg, key, salt);
-    return curve.g.mul(r); //.encode("array", true);
-  }
-
-  public static groupRandomPoint(
-    ...points: [string | Buffer | { x: BN; y: BN }][]
-  ) {
-    return points.reduce((point, current) => {
-      let currentPoint;
-      if (typeof current === "string") {
-        currentPoint = curve.decodePoint(Buffer.from(current, "hex"));
-      } else if (current instanceof Buffer) {
-        currentPoint = curve.decodePoint(current);
-      } else {
-        currentPoint = current;
+export function hash(...parts: (Buffer | BN | Point)[]): Buffer {
+  return hashOfBuffers(
+    ...parts.map(p => {
+      if (p instanceof Buffer) {
+        return p;
       }
 
-      if (!point) {
-        return currentPoint;
+      if (p instanceof BN) {
+        return p.toBuffer();
       }
 
-      return (<any>point).add(currentPoint);
-    }, null);
+      if (p.x && p.y) {
+        return Buffer.from(p.encode(16));
+      }
+
+      throw new Error("unknown type of part: " + typeof p);
+    })
+  );
+}
+
+export function L(Pn: Point[]): BN {
+  return hashInt(...Pn);
+}
+
+export function getAggregatePublicKey(L: BN, Xs: Point[]): Point {
+  let X = Xs[0].mul(hashInt(L, Xs[0]));
+
+  for (let i = 1; i < Xs.length; i++) {
+    X = X.add(Xs[i].mul(hashInt(L, Xs[i])));
   }
 
-  static sign(
-    hash: any,
-    pub: Buffer,
-    key: Buffer,
-    L: Buffer,
-    R: any,
-    P: any
-  ): any {
-    const k = musig.deterministinK(hash, key);
-    const h1 = new BN(
-      utils.hashOfBuffers(
-        Buffer.from(P.encode()),
-        Buffer.from(R.encode()),
-        hash
-      )
-    );
-    const h2 = new BN(utils.hashOfBuffers(L, pub));
-    const pk = new BN(key);
+  return X;
+}
 
-    return k.iadd(h1.mul(h2).mul(pk)).umod(curve.n);
+export function R(Pn: Point[]): Point {
+  let R = Pn[0];
+
+  for (let i = 1; i < Pn.length; i++) {
+    R = R.add(Pn[i]);
   }
 
-  static combine(s: BN[]) {
-    return s.reduce((total, current) => {
-      return total.iadd(current).umod(curve.n);
-    }, new BN(0));
+  return R;
+}
+
+export function getSi(
+  ri: BN,
+  X: Point,
+  R: Point,
+  m: Buffer,
+  L: BN,
+  xi: BN,
+  Xi: Point
+): MUSignature {
+  var s = hashInt(R, X, m)
+    .mul(hashInt(L, Xi))
+    .mul(xi);
+  var si = ri.add(s).umod(secp256k1.curve.n);
+  return {
+    r: R,
+    s: si
+  };
+}
+
+export function combine(sigs: MUSignature[], R: Point): MUSignature {
+  let S = sigs[0].s;
+
+  for (let i = 1; i < sigs.length; i++) {
+    S = S.add(sigs[i].s);
   }
 
-  static verify(hash: Buffer, s: BN, R: any, P: any) {
-    const H = new BN(
-      utils.hashOfBuffers(
-        Buffer.from(P.encode()),
-        Buffer.from(R.encode()),
-        hash
-      )
-    );
+  return {
+    r: R,
+    s: S
+  };
+}
 
-    return curve.g.mul(s).eq(R.add(P.mul(H)));
-  }
+export function verify(m: Buffer, s: MUSignature, X: Point, R: Point): boolean {
+  const lPoint = secp256k1.curve.g.mul(s.s);
+  const rPoint = R.add(X.mul(hashInt(R, X, m)));
+
+  return lPoint.eq(rPoint);
 }
